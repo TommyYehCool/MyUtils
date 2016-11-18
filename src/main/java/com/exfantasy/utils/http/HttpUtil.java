a877095e705a2804d56f133cff83f52983ba5b16
TommyYehCool <tommy.yeh1112@gmail.com>
2016-11-18 15:56:03 +0800

    Add sendFormPostRequest

--- src/main/java/com/exfantasy/utils/http/HttpUtil.java
+++ src/main/java/com/exfantasy/utils/http/HttpUtil.java
@@ -3,10 +3,13 @@ package com.exfantasy.utils.http;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -29,19 +32,21 @@ public class HttpUtil {
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
 
-			String responseData = null;
-			String output;
-			while ((output = br.readLine()) != null) {
-				responseData = output;
+			StringBuffer result = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+				result.append(line);
 			}
-			System.out.println("HttpStatusCode: " + httpStatusCode + ", response: " + responseData);
-			return responseData;
+			String responseDate = result.toString();
+			
+			System.out.println("HttpStatusCode: " + httpStatusCode + ", response: " + responseDate);
+			return responseDate;
 		} catch (IOException e) {
 			throw new HttpUtilException("IOException raised while sending HTTP GET request to url: " + url, e);
 		}
 	}
 	
-	public static String sendPostRequest(String url, String jsonData) throws HttpUtilException {
+	public static String sendJsonPostRequest(String url, String jsonData) throws HttpUtilException {
 		try {
 			HttpPost post = new HttpPost(url);
 			
@@ -58,15 +63,46 @@ public class HttpUtil {
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
 
-			String responseData = null;
-			String output;
-			while ((output = br.readLine()) != null) {
-				responseData = output;
+			StringBuffer result = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+				result.append(line);
 			}
-			System.out.println("HttpStatusCode: " + httpStatusCode + ", response: " + responseData);
-			return responseData;
+			String responseDate = result.toString();
+			
+			System.out.println("HttpStatusCode: " + httpStatusCode + ", response: " + responseDate);
+			return responseDate;
 		} catch (IOException e) {
-			throw new HttpUtilException("IOException raised while sending HTTP POST request to url: " + url, e);
+			throw new HttpUtilException("IOException raised while sending HTTP JSON POST request to url: " + url, e);
+		}
+	}
+	
+	public static String sendFormPostRequest(String url, List<NameValuePair> params) throws HttpUtilException {
+		try {
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpPost post = new HttpPost(url);
+	
+			post.setEntity(new UrlEncodedFormEntity(params));
+	
+			HttpResponse response = client.execute(post);
+			int httpStatusCode = response.getStatusLine().getStatusCode();
+			if (httpStatusCode != HttpStatus.SC_OK) {
+				throw new HttpUtilException("Failed - HTTP error code: " + httpStatusCode);
+			}
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+	
+			StringBuffer result = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+				result.append(line);
+			}
+			String responseDate = result.toString();
+			
+			System.out.println("HttpStatusCode: " + httpStatusCode + ", response: " + responseDate);
+			return responseDate;
+		} catch (IOException e) {
+			throw new HttpUtilException("IOException raised while sending HTTP JSON POST request to url: " + url, e);
 		}
 	}
 }
