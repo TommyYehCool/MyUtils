44a62ff239425eefdc6f5fa61d2e408e75406f8f
TommyYehCool <tommy.yeh1112@gmail.com>
2016-11-21 17:54:07 +0800

    調整時間轉換物件

--- src/main/java/com/exfantasy/utils/json/GsonLocalDateDeserializer.java
+++ src/main/java/com/exfantasy/utils/json/GsonLocalDateDeserializer.java
@@ -1,6 +1,8 @@
 package com.exfantasy.utils.json;
 
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -11,14 +13,21 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
 public class GsonLocalDateDeserializer implements JsonDeserializer<LocalDate> {
+	
+	private final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	@Override
 	public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
 			throws JsonParseException {
 		String strDate = json.getAsJsonPrimitive().getAsString();
-		Date date = new Date(Long.parseLong(strDate));
-		LocalDate lDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        return lDate;
+		try {
+			Date date = dateTimeFormat.parse(strDate);
+			LocalDate lDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	        return lDate;
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
